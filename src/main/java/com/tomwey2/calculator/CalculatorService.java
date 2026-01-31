6d7336143fc7f29c64696f33aa3d28f24fa57ce2
gat <tfa613@yahoo.de>
2026-01-31 22:30:50 +0100

    feat: Fixed the failing test case in CalculatorServiceTest.divideNegativ...

--- src/main/java/com/tomwey2/calculator/CalculatorService.java
+++ src/main/java/com/tomwey2/calculator/CalculatorService.java
@@ -7,4 +7,11 @@ public class CalculatorService {
     public int sum(int a, int b) {
         return a + b;
     }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
 }
